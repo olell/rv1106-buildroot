3197d0cb375485260b8a8a73135e220b241bb939
olel <olel@olel.de>
2024-12-07 10:44:06 +0100

    Implemented rootfs autobuild + getty on serial port

--- board/RV1106/boot.cmd
+++ board/RV1106/boot.cmd
@@ -1,1 +1,5 @@
-env set bootcmd "fatload mmc 1:4 0x8C00000 rv1106-custom.dtb;fatload mmc 1:4 0x8000800 zImage;bootz 0x8000800 - 0x8C00000"
+env set bootargs "console=ttyFIQ0,115200 panic=5 rootwait root=/dev/mmcblk1p6 rw"
+env set sys_bootargs "console=ttyFIQ0,115200 panic=5 rootwait root=/dev/mmcblk1p6 rw"
+fatload mmc 1:5 0x8C00000 rv1106-custom.dtb
+fatload mmc 1:5 0x8000800 zImage
+bootz 0x8000800 - 0x8C00000
