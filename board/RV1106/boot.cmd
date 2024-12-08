a07fe89fe60169fbe8b0ed9831e3c36d04128ef0
olel <olel@olel.de>
2024-12-08 10:46:29 +0100

    uboot: changed kernel + dtb load addresses to make more memory available

--- board/RV1106/boot.cmd
+++ board/RV1106/boot.cmd
@@ -1,5 +1,5 @@
 env set bootargs "console=ttyFIQ0,115200 panic=5 rootwait root=/dev/mmcblk1p6 rw"
 env set sys_bootargs "console=ttyFIQ0,115200 panic=5 rootwait root=/dev/mmcblk1p6 rw"
-fatload mmc 1:5 0x8C00000 rv1106-custom.dtb
-fatload mmc 1:5 0x8000800 zImage
-bootz 0x8000800 - 0x8C00000
+fatload mmc 1:5 0x808000 rv1106-custom.dtb
+fatload mmc 1:5 0x818000 zImage
+bootz 0x818000 - 0x808000
